b672b6f0ec4191cfa090797417cbf423ce13a378
paulo <paulo2261sousa@gmail.com>
2023-04-20 13:49:14 -0300

    session

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -4,6 +4,12 @@ class UserSessionsController < ApplicationController
     end
     
     def create
+        @session = UserSession.new(session, params[:user_session])
+        if @session.authenticate
+            redirect_to root_path, :notice => t('flash.notice.signed_in')
+        else
+            render :new
+        end
     end
     
     def destroy   
