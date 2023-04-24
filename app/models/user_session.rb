3690a8cb065d2fc109eac916e5e72d37ce39e55e
paulo <paulo2261sousa@gmail.com>
2023-04-24 19:36:21 -0300

    helpers de sessão

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -35,4 +35,11 @@ class UserSession
         false
     end
 
+    def current_user
+        User.find(@session[:user_id])
+    end
+
+    def user_signed_in?
+        @session[:user_id].present?
+    end
 end
